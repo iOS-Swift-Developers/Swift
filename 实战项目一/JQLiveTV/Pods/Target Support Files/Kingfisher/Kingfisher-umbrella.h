99e0675c83cdd5b60b249d67b1518c7dbc270f45
HaRi <hjq99@163.com>
2017-08-01 09:32:29 +0800

    fix: 2017.08.01 今日新增: 实战篇实战项目-JQLiveTV实战项目一结束

--- 实战项目一/JQLiveTV/Pods/Target Support Files/Kingfisher/Kingfisher-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "Kingfisher.h"
-
-FOUNDATION_EXPORT double KingfisherVersionNumber;
-FOUNDATION_EXPORT const unsigned char KingfisherVersionString[];
-
--- 实战项目一/JQLiveTV/Pods/Target Support Files/Pods-JQLiveTV/Pods-JQLiveTV-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JQLiveTV : NSObject
-@end
-@implementation PodsDummy_Pods_JQLiveTV
-@end

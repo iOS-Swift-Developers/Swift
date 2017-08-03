b0767c7ae71844d5b283efe21e011be95c1c2bb5
HaRi <hjq99@163.com>
2017-08-03 09:31:03 +0800

    fix: 2017.08.03 今日新增: RxSwift-完善注释!

--- RxSwift使用教程/6-RxSwift的UITableVIew使用/6-RxSwift的UITableVIew使用/HeroViewModel.swift
+++ RxSwift使用教程/6-RxSwift的UITableVIew使用/6-RxSwift的UITableVIew使用/HeroViewModel.swift
@@ -9,16 +9,46 @@
 import UIKit
 import RxSwift
 
+
 class HeroViewModel {
     
-    var herosObserable : Variable<[Hero]> = {
+    fileprivate lazy var bag : DisposeBag = DisposeBag()
+    
+    lazy var heroVariable : Variable<[Hero]> = {
+       return Variable(self.getHeroData())
+    }()
+    
+    var searchText : Observable<String>
+    
+    init(searchText : Observable<String>) {
+        self.searchText = searchText
         
-       let path = Bundle.main.path(forResource: "heros.plist", ofType: nil)!
+        self.searchText.subscribe(onNext: { (str : String) in
+            let heros = self.getHeroData().filter({ (hero : Hero) -> Bool in
+                if str == "" { return  true }
+                return hero.name.contains(str)
+            })
+            self.heroVariable.value = heros
+        }).addDisposableTo(bag)
+    }
+    deinit {
+        print("~~~")
+    }
+}
+
+extension HeroViewModel {
+    fileprivate func getHeroData() -> [Hero] {
+        // 1.获取路径
+        let path = Bundle.main.path(forResource: "heros.plist", ofType: nil)!
+        
+        // 2.读取文件内容
         let dataArray = NSArray(contentsOfFile: path) as! [[String : Any]]
+        
+        // 3.遍历所有的字典并且转成模型对象
         var heros : [Hero] = [Hero]()
         for dict in dataArray {
             heros.append(Hero(dict: dict))
         }
-        return Variable(heros)
-    }()
+        return heros
+    }
 }
--- RxSwift使用教程/6-RxSwift的UITableVIew使用/6-RxSwift的UITableVIew使用/ViewController.swift
+++ RxSwift使用教程/6-RxSwift的UITableVIew使用/6-RxSwift的UITableVIew使用/ViewController.swift
@@ -12,24 +12,35 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     fileprivate lazy var bag : DisposeBag = DisposeBag()
-    fileprivate var heros : [Hero]?
+    fileprivate var heros : [Hero] = [Hero]()
     
-    let heroVM = HeroViewModel() // 创建ViewModel
+    fileprivate lazy var heroVM : HeroViewModel = HeroViewModel(searchText:self.searchText)
+    var searchText: Observable<String> {
+        return searchBar.rx.text.orEmpty.throttle(0.5, scheduler: MainScheduler.instance)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        heroVM.herosObserable.asObservable().bind(to: tableView.rx.items(cellIdentifier:"HeroCellID", cellType: UITableViewCell.self)) { row, hero, cell in
+        // 1.取出调整底部内边距
+        automaticallyAdjustsScrollViewInsets = false
+        
+        // 2.给tableView绑定数据
+        heroVM.heroVariable.asObservable().bind(to: tableView.rx.items(cellIdentifier:"HeroCellID", cellType: UITableViewCell.self)) { row, hero, cell in
             cell.textLabel?.text = hero.name
             cell.detailTextLabel?.text = hero.intro
             cell.imageView?.image = UIImage.init(named: hero.icon)
         }.addDisposableTo(bag)
         
+        //  3.监听UITableView的点击
         tableView.rx.itemSelected.subscribe(onNext: { (indexPath) in
             print(indexPath)
         }).addDisposableTo(bag)
+        
         
         tableView.rx.modelSelected(Hero.self).subscribe(onNext: { (hero : Hero) in
             print(hero.name)
@@ -44,7 +55,7 @@ class ViewController: UIViewController {
     
     @IBAction func itemAction(_ sender: UIBarButtonItem) {
         let hero = Hero(dict: ["icon" : "nss", "name" : "xiaohange", "intro" : "最强王者!!"])
-        heroVM.herosObserable.value = [hero]
+        heroVM.heroVariable.value = [hero]
     }
 
 }
